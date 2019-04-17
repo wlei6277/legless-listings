167c8d72268be7584483a777420a05f402b8fa46
Billy Leitch <wlei6277@gmail.com>
2019-04-17 14:16:36 +1000

    added the snake gem and added some data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Snake.names.each do |name|
+    Breed.create(name: name.downcase.strip)
+    puts "#{name} created"
+end
