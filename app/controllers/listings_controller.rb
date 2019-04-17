b59cc5c75af7b49365dd40d21dbccc6cbc104e24
Billy Leitch <wlei6277@gmail.com>
2019-04-17 15:25:42 +1000

    added in methods to controller and before action

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,2 +1,35 @@
 class ListingsController < ApplicationController
+    before_action :set_listing, only: [:show, :edit, :update, :destroy]
+    def index
+    
+    end
+
+    def show
+        
+    end
+
+    def new
+    
+    end
+    
+    def create
+    
+    end
+
+    def edit
+        
+    end
+
+    def update
+        
+    end
+    
+    def destroy
+        
+    end
+    
+    def set_listing
+        id = params[:id]
+        @listing = Listing.find(id)
+    end
 end
