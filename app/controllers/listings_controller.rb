7f051340064400543872667391a2dacb095cd1aa
Billy Leitch <wlei6277@gmail.com>
2019-04-26 10:41:21 +1000

    added in Stripe gem and modified the show view with the deposit button

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,6 +1,7 @@
 class ListingsController < ApplicationController
-    before_action :authenticate_user!
+    before_action :authenticate_user!, except: [:index, :show]
     before_action :set_listing, only: [:show, :edit, :update, :destroy]
+    before_action :authorize_user, only: [:edit, :update, :destroy]
     before_action :set_breeds_and_sexes, only: [:new, :edit]
 
     def index
@@ -16,9 +17,9 @@ class ListingsController < ApplicationController
     end
     
     def create
-        @listing = Listing.create(listing_params)
+        @listings = current_user.listings.create(listing_params)
 
-        if @listing.errors.any?
+        if @listings.errors.any?
             set_breeds_and_sexes
             render "new"
         else
@@ -43,6 +44,12 @@ class ListingsController < ApplicationController
     def set_listing
         id = params[:id]
         @listing = Listing.find(id)
+    end
+    
+    def authorize_user
+        if @listing.user_id != current_user.id
+            redirect_to listings_path
+        end
     end
 
     def listing_params
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,5 +1,6 @@
 class Listing < ApplicationRecord
   belongs_to :breed
+  belongs_to :user
   enum sex: {female: 0, male: 1}
   validates :title, :breed_id, :sex, :price, :deposit, :date_of_birth, presence: true
   has_one_attached :picture
